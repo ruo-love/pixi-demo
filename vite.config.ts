dda9d364c7a8e7fe72bd4daa32a417243d3942b6
qiancheng.zhao <qiancheng.zhao@alo7.com>
2026-01-27 19:42:28 +0800

    feat: upt

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
 
 import { assetpackPlugin } from "./scripts/assetpack-vite-plugin";
+import legacy from "@vitejs/plugin-legacy"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [assetpackPlugin()],
+  base:"./",
+  root:"./",
+  plugins: [assetpackPlugin(),legacy({
+            targets: [
+                "last 2 versions",
+                "iOS >= 10",
+                "Android >= 6",
+                "Chrome >= 49",
+                "Safari >= 10",
+                "Samsung >= 5",
+                "OperaMobile >= 46",
+                // 其他特定版本或者范围
+            ],
+        })],
   server: {
+    host:"0.0.0.0",
     port: 8080,
     open: true,
   },
